3f7fef866e7416405f51f0cba52835c482acb043
Leohh <948750310@qq.com>
2023-06-12 05:29:41 +0800

    fix: delete useless scripts

--- scripts/download_dataset.sh
+++ scripts/download_dataset.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-mkdir datasets
-cd datasets
+mkdir dataset
+cd dataset
 # Download describable textures dataset
 wget https://www.robots.ox.ac.uk/~vgg/data/dtd/download/dtd-r1.0.1.tar.gz
 tar -xf dtd-r1.0.1.tar.gz
